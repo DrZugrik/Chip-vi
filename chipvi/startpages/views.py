1a8363eecf9848dee7a05bc10d6e6a2111e164b8
DrZugrik <i.zugrik@gmail.com>
2022-03-03 17:52:47 +0300

    Register form

--- chipvi/startpages/views.py
+++ chipvi/startpages/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+
+from .forms import UserRegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,14 +17,24 @@ def faq(request):
 def donate(request):
     return render(request, 'startpages/donate.html')
 
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-    else:
-        form = UserCreationForm()
-    return render(request, 'startpages/register.html', {"form": form})
+
+
 
 def login(request):
     return render(request, 'startpages/login.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.error(request, 'егистрация пройдена!!!')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'startpages/register.html', {"form": form})
+
+
+
